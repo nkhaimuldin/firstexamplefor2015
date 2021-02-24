53b7da795d2a513ed117fc301f58df67de858860
nkhaimuldin <n.khaimuldin@astanait.edu.kz>
2021-02-24 14:58:51 +0600

    first commit (init)

---
+++ src/com/company/repositories/interfaces/IUserRepository.java
@@ -0,0 +1,12 @@
+package com.company.repositories.interfaces;
+
+import com.company.entities.User;
+
+import java.util.List;
+
+public interface IUserRepository {
+    boolean createUser(User user);
+    User getUser(int id);
+    List<User> getAllUsers();
+    User getUserByName(String name);
+}
